c4e0af41ce48a46ec5e950277e7630f9b526f7f1
Reytler Souza <reytlercompare@hotmail.com>
2022-09-28 13:46:42 -0300

    Criação do CreateUserController e do método de envio de email no CreateUserUseCase

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
     constructor(
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider,
     ){}
 
     async execute(data: ICreateUserRequestDTO){
@@ -17,5 +19,18 @@ export class CreateUserUseCase {
         const user = new User(data);
 
         await this.usersRepository.save(user);
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email
+            },
+            from: {
+                name: 'Serviço de criação de usuário',
+                email: 'master@master.com'
+            },
+            subject: 'Seja bem vindo à plataforma',
+            body: '<p>Você já pode fazer login em nossa plataforma.</p>'
+        })
     }
 }
